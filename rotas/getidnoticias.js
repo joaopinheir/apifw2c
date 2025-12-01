ab05ccadad11e0ccdc15a08a85e20ee3e67badf1
joaopinheir <149428094+joaopinheir@users.noreply.github.com>
2025-12-01 17:21:35 -0400

    Add files via upload

--- rotas/getidnoticias.js
+++ rotas/getidnoticias.js
@@ -1,5 +1,18 @@
-module.exports=(app)=>{
-    app.get("noticias/id/:id",async (req,res)=>{
-        res.send(req.params.id)
+const { ObjectId } = require("mongodb")
+
+module.exports = (app) => {
+    app.get("/noticias/id/:id", async (req, res) => {
+        try {
+            const id = ObjectId.createFromHexString(req.params.id)
+            await app.dbClient.connect();
+            const resultado = await app.dbClient.db('portalnoticias')
+                .collection('noticias')
+                .find({ _id: id })
+                .toArray();
+            res.json(resultado);
+        } catch (error) {
+            res.json(error);
+
+        }
     })
 }
--- rotas/gettiponoticias.js
+++ rotas/gettiponoticias.js
@@ -1,5 +1,16 @@
-module.exports=(app)=>{
-    app.get("nottipocias/tipo/:tiponoticia",async (req,res)=>{
-        res.send(req.params.id)
+module.exports = (app) => {
+    app.get("/noticias/tipo/:tiponoticia", async (req, res) => {
+        try {
+            const tiponoticia = ObjectId.createFromHexString(req.params.tiponoticia)
+            await app.dbClient.connect();
+            const resultado = await app.dbClient.db('portalnoticias')
+                .collection('noticias')
+                .find({ tiponoticia: tiponoticia })
+                .toArray();
+            res.json(resultado);
+        } catch (error) {
+            res.json(error);
+
+        }
     })
 }
