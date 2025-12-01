d411423bf95ee07e8252c5687945bfc0385c683a
joaopinheir <149428094+joaopinheir@users.noreply.github.com>
2025-12-01 17:29:10 -0400

    Add files via upload

--- rotas/gettiponoticias.js
+++ rotas/gettiponoticias.js
@@ -1,11 +1,11 @@
 module.exports = (app) => {
     app.get("/noticias/tipo/:tiponoticia", async (req, res) => {
         try {
-            const tiponoticia = ObjectId.createFromHexString(req.params.tiponoticia)
+            const tiponoticia = req.params.tiponoticia
             await app.dbClient.connect();
             const resultado = await app.dbClient.db('portalnoticias')
                 .collection('noticias')
-                .find({ tiponoticia: tiponoticia })
+                .find({ tiponoticia: new RegExp( tiponoticia,i) })
                 .toArray();
             res.json(resultado);
         } catch (error) {
