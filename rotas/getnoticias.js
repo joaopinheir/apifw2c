888af5aa98adeec8fbc15c103ed38af772e88aa7
joaopinheir <149428094+joaopinheir@users.noreply.github.com>
2025-12-01 16:41:33 -0400

    Add files via upload

--- rotas/getnoticias.js
+++ rotas/getnoticias.js
@@ -2,11 +2,11 @@ module.exports = (app) => {
   app.get("/noticias", async (req, res) => {
     try {
       await app.dbClient.connect();
-      const users = await app.dbClient.db('portal')
+      const resultado = await app.dbClient.db('portalnoticias')
             .collection('noticias')
             .find()
             .toArray();
-      res.json(users);
+      res.json(resultado);
     } catch (error) {
       res.send("erro:" + error)
     } finally {
